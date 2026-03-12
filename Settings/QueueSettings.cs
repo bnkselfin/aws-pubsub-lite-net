eec235d6b6fe8c4f6af9095d2fbe3830893bb644
Bunyamin Abbasov <bnkselfin@gmail.com>
2026-03-12 20:30:00 +0400

    adds queue settings

---
+++ Settings/QueueSettings.cs
@@ -0,0 +1,11 @@
+namespace AwsPubSubLite.Settings;
+
+public sealed class QueueSettings
+{
+    public int MaxMessageCount { get; set; } = 10;
+    public int RetryCount { get; set; } = 3;
+    public long RetryIntervalMs { get; set; } = 1000;
+    public long MessageRetentionMs { get; set; } = 345_600_000;
+    public int WaitTimeSeconds { get; set; } = 20;
+    public int VisibilityTimeoutSecs { get; set; } = 30;
+}
