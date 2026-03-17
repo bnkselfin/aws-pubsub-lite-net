72c51a4c61aaaa9972076c802412f02b92407225
Bunyamin Abbasov <bnkselfin@gmail.com>
2026-03-17 11:30:00 +0400

    adds dlq retention to queue settings

--- Settings/QueueSettings.cs
+++ Settings/QueueSettings.cs
@@ -8,4 +8,5 @@ public sealed class QueueSettings
     public long MessageRetentionMs { get; set; } = 345_600_000;
     public int WaitTimeSeconds { get; set; } = 20;
     public int VisibilityTimeoutSecs { get; set; } = 30;
+    public long DlqMessageRetentionMs { get; set; } = 1_209_600_000;
 }
