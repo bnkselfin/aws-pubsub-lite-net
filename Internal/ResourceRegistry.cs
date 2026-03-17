c0938ecc90207b9e23f6719bd7a2ec33c5a335de
Bunyamin Abbasov <bnkselfin@gmail.com>
2026-03-17 20:55:00 +0400

    adds dlq slot to resource registry

--- Internal/ResourceRegistry.cs
+++ Internal/ResourceRegistry.cs
@@ -8,15 +8,20 @@ internal sealed class ResourceRegistry
 {
     private readonly ConcurrentDictionary<string, TopicResource> _topics = new();
     private readonly ConcurrentDictionary<string, QueueResource> _queues = new();
+    private readonly ConcurrentDictionary<string, DlqHandle> _dlqs = new();
 
     public IReadOnlyDictionary<string, TopicResource> Topics => _topics;
     public IReadOnlyDictionary<string, QueueResource> Queues => _queues;
+    public IReadOnlyDictionary<string, DlqHandle> Dlqs => _dlqs;
 
     public TopicResource GetOrAddTopic(string name, TopicResource topic) =>
         _topics.GetOrAdd(name, topic);
 
     public QueueResource GetOrAddQueue(string name, QueueResource queue) =>
         _queues.GetOrAdd(name, queue);
+
+    public DlqHandle GetOrAddDlq(string name, DlqHandle dlq) =>
+        _dlqs.GetOrAdd(name, dlq);
 
     public bool TryGetTopic(string name, [MaybeNullWhen(false)] out TopicResource topic) =>
         _topics.TryGetValue(name, out topic);
