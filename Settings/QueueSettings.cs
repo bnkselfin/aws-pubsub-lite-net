bddaebcd7ce959f50c25319a86b9d9851524a317
Bunyamin Abbasov <bnkselfin@gmail.com>
2026-04-06 13:20:00 +0400

    Fixes queue settings to validate ranges

--- Settings/QueueSettings.cs
+++ Settings/QueueSettings.cs
@@ -2,11 +2,67 @@ namespace AwsPubSubLite.Settings;
 
 public sealed class QueueSettings
 {
-    public int MaxMessageCount { get; set; } = 10;
-    public int RetryCount { get; set; } = 3;
-    public long RetryIntervalMs { get; set; } = 1000;
-    public long MessageRetentionMs { get; set; } = 345_600_000;
-    public int WaitTimeSeconds { get; set; } = 20;
-    public int VisibilityTimeoutSecs { get; set; } = 30;
-    public long DlqMessageRetentionMs { get; set; } = 1_209_600_000;
+    private int _maxMessageCount = 10;
+    private int _retryCount = 3;
+    private long _retryIntervalMs = 1000;
+    private long _messageRetentionMs = 345_600_000;
+    private int _waitTimeSeconds = 20;
+    private int _visibilityTimeoutSecs = 30;
+    private long _dlqMessageRetentionMs = 1_209_600_000;
+
+    public int MaxMessageCount
+    {
+        get => _maxMessageCount;
+        set => _maxMessageCount = value is >= 1 and <= 10
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be between 1 and 10.");
+    }
+
+    public int RetryCount
+    {
+        get => _retryCount;
+        set => _retryCount = value >= 0
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be non-negative.");
+    }
+
+    public long RetryIntervalMs
+    {
+        get => _retryIntervalMs;
+        set => _retryIntervalMs = value > 0
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive.");
+    }
+
+    public long MessageRetentionMs
+    {
+        get => _messageRetentionMs;
+        set => _messageRetentionMs = value > 0
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive.");
+    }
+
+    public int WaitTimeSeconds
+    {
+        get => _waitTimeSeconds;
+        set => _waitTimeSeconds = value is >= 0 and <= 20
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be between 0 and 20.");
+    }
+
+    public int VisibilityTimeoutSecs
+    {
+        get => _visibilityTimeoutSecs;
+        set => _visibilityTimeoutSecs = value is >= 0 and <= 43200
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be between 0 and 43200.");
+    }
+
+    public long DlqMessageRetentionMs
+    {
+        get => _dlqMessageRetentionMs;
+        set => _dlqMessageRetentionMs = value > 0
+            ? value
+            : throw new ArgumentOutOfRangeException(nameof(value), value, "Must be positive.");
+    }
 }
